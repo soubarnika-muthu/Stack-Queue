0709f5d8fdb8ae0b78d21252395cfbb57e119a57
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-10 21:11:56 +0530

    [soubarnika]Adding Push Method

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -2,11 +2,20 @@
 
 namespace StackAndQueue
 {
+    //UC1 Adding Push Operation
     class Program
     {
+   
         static void Main(string[] args)
         {
             Console.WriteLine("Implementation of stack and Queue");
+            //creating object for stack
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(53);
+            stack.Display();
+            Console.WriteLine("Stack is created and elements are pushed");
         }
     }
 }
---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class StackNode
+    {
+        public int value;
+        public StackNode Next;
+        public StackNode(int value)
+        {
+            this.value = value;
+            this.Next = null;
+        }
+    }
+    class Stack
+    {
+        public StackNode top;
+
+        public void Push(int value)
+        {
+            StackNode newNode = new StackNode(value);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+        }
+        public void Display()
+        {
+            StackNode temp = this.top;
+            if(top==null)
+            {
+                Console.WriteLine("No elements in the Stack ");
+            }
+           while(temp!=null)
+            {
+                Console.WriteLine("Value in the Stack:"+temp.value);
+                temp = temp.Next;
+            }
+        }
+
+    }
+
+}
+ 
+
